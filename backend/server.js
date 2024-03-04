21a515d8d7cb318bf52861e5bd0b34ab98f6445a
sifirsikinti <celalakin1977@gmail.com>
2024-03-04 20:25:28 +0100

    Flüge und Hotels

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,23 @@
-const express = require('express');
-const axios = require('axios');
-const qs = require('qs');
-const cors = require('cors');
+const express = require("express");
+const axios = require("axios");
+const qs = require("qs");
+const cors = require("cors");
+const Amadeus = require("amadeus");
 
 const app = express();
 app.use(cors()); // Ermögliche CORS für alle Anfragen
+app.use(express.json());
 
 const port = 3000;
 
-const AMADEUS_CLIENT_ID = 'VGrjvtJs0sokLE1WWuUDYlvnMmL1ake1';
-const AMADEUS_CLIENT_SECRET = 'MsSt3tV5ENRR3uQl';
+const AMADEUS_CLIENT_ID = "VGrjvtJs0sokLE1WWuUDYlvnMmL1ake1";
+const AMADEUS_CLIENT_SECRET = "MsSt3tV5ENRR3uQl";
+
+const amadeus = new Amadeus({
+  // Initialisieren Sie die Amadeus-API-Bibliothek mit Ihren Anmeldeinformationen
+  clientId: AMADEUS_CLIENT_ID,
+  clientSecret: AMADEUS_CLIENT_SECRET,
+});
 
 let cachedToken = {
   value: null,
@@ -22,46 +30,145 @@ async function getAccessToken() {
     return cachedToken.value;
   }
 
-  const tokenUrl = 'https://test.api.amadeus.com/v1/security/oauth2/token';
+  const tokenUrl = "https://test.api.amadeus.com/v1/security/oauth2/token";
   const data = qs.stringify({
     client_id: AMADEUS_CLIENT_ID,
     client_secret: AMADEUS_CLIENT_SECRET,
-    grant_type: 'client_credentials',
+    grant_type: "client_credentials",
   });
 
   try {
     const response = await axios.post(tokenUrl, data, {
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+        "Content-Type": "application/x-www-form-urlencoded",
       },
     });
-    const expiry = new Date(now.getTime() + response.data.expires_in * 1000 - 60000); // Adjust expiry time
+    const expiry = new Date(
+      now.getTime() + response.data.expires_in * 1000 - 60000
+    ); // Adjust expiry time
     cachedToken = {
       value: response.data.access_token,
       expiry: expiry,
     };
-    console.log('Access Token:', response.data.access_token);
+    console.log("Access Token:", response.data.access_token);
     return response.data.access_token;
   } catch (error) {
-    console.error('Fehler beim Anfordern des Access Tokens:', error.response ? error.response.data : error.message);
+    console.error(
+      "Fehler beim Anfordern des Access Tokens:",
+      error.response ? error.response.data : error.message
+    );
     throw error; // Für außerhalb der Funktion sichtbar machen
   }
 }
 
+app.get("/search-cities", async (req, res) => {
+  try {
+    const accessToken = await getAccessToken();
+    const keyword = req.query.keyword;
+    const amadeusResponse = await axios.get(
+      `https://test.api.amadeus.com/v1/reference-data/locations?subType=CITY&keyword=${keyword}`,
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    );
 
-app.get('/search-cities', async (req, res) => {
-    try {
-      const accessToken = await getAccessToken();
-      const keyword = req.query.keyword;
-      const amadeusResponse = await axios.get(`https://test.api.amadeus.com/v1/reference-data/locations?subType=CITY&keyword=${keyword}`, {
-        headers: { 'Authorization': `Bearer ${accessToken}` },
-      });
-  
-      res.json(amadeusResponse.data);
-    } catch (error) {
-      console.error('Fehler bei der Städtesuche:', error);
-      res.status(500).json({ message: 'Interner Serverfehler', error: error.toString() });
+    res.json(amadeusResponse.data);
+  } catch (error) {
+    console.error("Fehler bei der Städtesuche:", error);
+    res
+      .status(500)
+      .json({ message: "Interner Serverfehler", error: error.toString() });
+  }
+});
+
+app.post("/search-flights", async (req, res) => {
+  // Ihre existierenden Parameter…
+  const {
+    departure,
+    locationDeparture,
+    locationArrival,
+    adults,
+    children = 0,
+    infants = 0,
+    travelClass = "ECONOMY",
+    nonStop = false,
+    maxPrice,
+    returnDate,
+  } = req.body;
+
+  try {
+    const accessToken = await getAccessToken(); // Zugriffstoken abrufen
+
+    // Parametervorbereitung für die API-Anfrage
+    let searchParams = {
+      originLocationCode: locationDeparture,
+      destinationLocationCode: locationArrival,
+      departureDate: departure,
+      adults: parseInt(adults),
+      travelClass: travelClass,
+      nonStop: nonStop,
+      returnDate: returnDate || undefined,
+    };
+
+    if (children > 0) searchParams.children = parseInt(children);
+    if (infants > 0) searchParams.infants = parseInt(infants);
+    if (maxPrice) searchParams.maxPrice = maxPrice;
+
+    const amadeusResponse = await axios.get(
+      `https://test.api.amadeus.com/v2/shopping/flight-offers`,
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+        params: searchParams,
+      }
+    );
+
+    let flights = amadeusResponse.data.data;
+
+    // Filtern Sie Flüge basierend auf maxPrice, wenn angegeben
+    if (maxPrice) {
+      flights = flights.filter(
+        (flight) => parseFloat(flight.price.total) <= parseFloat(maxPrice)
+      );
     }
+
+    // Sortieren Sie die Flüge nach Preis und begrenzen Sie das Ergebnis auf die 10 günstigsten
+    flights.sort(
+      (a, b) => parseFloat(a.price.total) - parseFloat(b.price.total)
+    );
+    flights = flights.slice(0, 10);
+
+    res.json(flights);
+  } catch (error) {
+    console.error(
+      "Error searching flights:",
+      error.response ? error.response.data : error.message
+    );
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.toString() });
+  }
+});
+
+app.post("/search-hotels", async (req, res) => {
+  const { location } = req.body;
+  try {
+    const accessToken = await getAccessToken(); // Stellen Sie sicher, dass Sie einen gültigen Zugriffstoken haben
+    const hotelListUrl = `https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city?cityCode=${location}`;
+    const response = await axios.get(hotelListUrl, {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+
+    const hotels = response.data.data.slice(0, 10);
+    res.json(hotels);
+  } catch (error) {
+    console.error(
+      "Fehler bei der Hotelsuche:",
+      error.response ? error.response.data : error.message
+    );
+    res
+      .status(500)
+      .json({ message: "Interner Serverfehler", error: error.toString() });
+  }
 });
 
 app.listen(port, () => {
--- src/components/StartingPage.vue
+++ src/components/StartingPage.vue
@@ -7,118 +7,364 @@
 
     <div class="routePlanner">
       <h1>Dein Reiseplaner</h1>
-      
-      <!-- Startort InputGroup -->
-      <div class="inputGroup">
-        <div class="inputWithIcon">
-          <span class="icon">&#9992;</span> <!-- Flugzeug Icon -->
-          <input v-model="cityName" @input="searchCities" type="text" placeholder="Startort" />
-        </div>
-        <div class="dropdownContainer" v-if="cities.length">
-          <div class="dropdown">
-            <div v-for="city in cities" :key="city.id" @click="selectCity(city)" class="dropdown-item">
-              {{ city.name }}
+      <div class="mainInputs">
+        <div class="inputGroup">
+          <div class="inputWithIcon">
+            <span class="icon">&#9992;</span>
+            <input
+              v-model="cityName"
+              @input="searchCities"
+              type="text"
+              placeholder="Startort*"
+            />
+          </div>
+          <div class="dropdownContainer" v-if="cities.length">
+            <div class="dropdown">
+              <div
+                v-for="city in cities"
+                :key="city.id"
+                @click="selectCity(city)"
+                class="dropdown-item"
+              >
+                {{ city.name }}
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <!-- Zielort InputGroup -->
-      <div class="inputGroup">
-        <div class="inputWithIcon">
-          <span class="icon">&#9992;</span> <!-- Flugzeug Icon -->
-          <input v-model="destinationName" @input="searchDestinations" type="text" placeholder="Zielort" />
-        </div>
-        <div class="dropdownContainer" v-if="destinations.length">
-          <div class="dropdown">
-            <div v-for="destination in destinations" :key="destination.id" @click="selectDestination(destination)" class="dropdown-item">
-              {{ destination.name }}
+        <div class="inputGroup">
+          <div class="inputWithIcon">
+            <span class="icon">&#9992;</span>
+            <input
+              v-model="destinationName"
+              @input="searchDestinations"
+              type="text"
+              placeholder="Zielort*"
+            />
+          </div>
+          <div class="dropdownContainer" v-if="destinations.length">
+            <div class="dropdown">
+              <div
+                v-for="destination in destinations"
+                :key="destination.id"
+                @click="selectDestination(destination)"
+                class="dropdown-item"
+              >
+                {{ destination.name }}
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <button class="plan">
+        <div class="inputGroup hinflug">
+          <h4>Hinflugdatum:*</h4>
+          <input
+            class="Datum"
+            v-model="departure"
+            type="date"
+            placeholder="Anfangsdatum"
+          />
+        </div>
+
+        <div class="inputGroup erwachsene">
+          <h4>Anzahl Erwachsene (älter 12 Jahre):*</h4>
+          <input
+            class="Datum"
+            v-model="adults"
+            type="number"
+            placeholder="Anzahl Erwachsene"
+          />
+        </div>
+      </div>
+      <button class="filterIcon" @click="toggleAdvancedOptions">
+        <span class="buttonIcon"> &#x2630;</span>Details
+      </button>
+
+      <div v-if="showAdvancedOptions">
+        <div class="erwachsene">
+        <h4 for="departureDate">Anzahl Kinder</h4>
+        <input
+          class="Datum"
+          v-model="children"
+          type="number"
+          placeholder="Anzahl Kinder"
+        />
+        </div>
+        <div class="erwachsene">
+        <h4 for="departureDate">Anzahl Kinder jünger 2 Jahre</h4>
+        <input
+          class="Datum"
+          v-model="infants"
+          type="number"
+          placeholder="Anzahl Kinder < 2 Jahre"
+        />
+</div>
+<div class="erwachsene">
+        <h4 for="departureDate">Rückflugdatum</h4>
+
+        <input
+          class="Datum"
+          v-model="returnDate"
+          type="date"
+          placeholder="Rückflugdatum"
+        />
+        </div>
+
+        <div class="inputGroup">
+          <h4 for="departureDate" class="reiseklasse">Reiseklasse</h4>
+          <div class="digga">
+            <select v-model="travelClass">
+              <option value="ECONOMY">Economy</option>
+              <option value="PREMIUM_ECONOMY">Premium Economy</option>
+              <option value="BUSINESS">Business</option>
+              <option value="FIRST">First</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="inputGroup checkboxGroup">
+            <label for="nonStop" class="checkboxLabel"
+              >Nur Non-Stop Flüge</label
+            >
+            <input
+              class="checkbox"
+              type="checkbox"
+              id="nonStop"
+              v-model="nonStop"
+            />
+          </div>
+
+        <div class="inputGroup erwachsene">
+          <input
+            class="max"
+            type="number"
+            v-model.number="maxPrice"
+            placeholder="Maximaler Preis"
+          />
+        </div>
+      </div>
+      <button class="plan" @click="searchFlightsAndHotels">
         <span class="buttonIcon">&#128269;</span> Suchen
       </button>
+
+      <button class="plan" @click="toggleFilter">
+        <font-awesome-icon icon="filter" class="buttonIcon"></font-awesome-icon
+        >{{ filterType }}
+      </button>
+    </div>
+
+    <div class="flight-container" v-if="filterType === 'Flüge'">
+      <div v-for="(flight, index) in flights" :key="index" class="flight-info">
+        <p class="flight-detail">
+          <strong>Abflug:</strong>
+          {{ formatDate(flight.itineraries[0].segments[0].departure.at) }}
+        </p>
+        <p class="flight-detail">
+          <strong>Ankunft:</strong>
+          {{ formatDate(flight.itineraries[0].segments[0].arrival.at) }}
+        </p>
+        <p class="flight-detail">
+          <strong>Preis:</strong> {{ flight.price.total }}
+          {{ flight.price.currency }}
+        </p>
+        <p class="flight-detail">
+          <strong>Fluggesellschaft:</strong>
+          {{ flight.itineraries[0].segments[0].carrierCode }}
+        </p>
+        <p class="flight-detail">
+          <strong>Flugnummer:</strong>
+          {{ flight.itineraries[0].segments[0].number }}
+        </p>
+        <p class="flight-detail">
+          <strong>Flugzeugtyp:</strong>
+          {{ flight.itineraries[0].segments[0].aircraft.code }}
+        </p>
+      </div>
+    </div>
+    <div class="hotel-container" v-else>
+      <div v-for="(hotel, index) in hotels" :key="index" class="hotel-info">
+        <p><strong>Name:</strong> {{ hotel.name }}</p>
+      </div>
     </div>
   </div>
 </template>
 
-
-
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   name: "StartingPage",
   data() {
-  return {
-    cityName: '',
-    cities: [],
-    destinationName: '', // Neuer v-model Wert für das Zielland
-    destinations: [], // Liste der Vorschläge für das Zielland
-  };
-},
-methods: {
-  async searchCities() {
-    if (this.cityName.length < 3) {
-      this.cities = []; // Leere die Liste, wenn weniger als 3 Buchstaben eingegeben wurden
-      return; // Beende die Methode vorzeitig
-    }
+    return {
+      cityName: "",
+      cities: [],
+      destinationName: "",
+      destinations: [],
+      flights: [],
+      departure: "", // Neu: Variable für das Abfahrtsdatum
+      arrival: "", // Neu: Variable für das Ankunftsdatum
+      hotels: [],
+      adults: 1, // Standardwert für Erwachsene
+      children: 0, // Standardwert für Kinder
+      infants: 0,
+      returnDate: "",
+      travelClass: "ECONOMY",
+      nonStop: false,
+      maxPrice: "",
+      showAdvancedOptions: false,
+      filterType: "Flüge",
+    };
+  },
 
-    try {
-      const response = await axios.get(`http://localhost:3000/search-cities?keyword=${this.cityName}`);
-      this.cities = response.data.data; // Aktualisiere die Liste mit den neuen Vorschlägen
-    } catch (error) {
-      console.error('Fehler beim Abrufen der Städte:', error);
-      this.cities = []; // Leere die Liste im Fehlerfall
-    }
-  },
-  async searchDestinations() {
-    if (this.destinationName.length < 3) {
-      this.destinations = []; // Leere die Liste, wenn weniger als 3 Buchstaben eingegeben wurden
-      return; // Beende die Methode vorzeitig
-    }
-
-    try {
-      const response = await axios.get(`http://localhost:3000/search-cities?keyword=${this.destinationName}`);
-      this.destinations = response.data.data; // Aktualisiere die Liste mit den neuen Vorschlägen
-    } catch (error) {
-      console.error('Fehler beim Abrufen der Ziele:', error);
-      this.destinations = []; // Leere die Liste im Fehlerfall
-    }
-  },
-  selectCity(city) {
-    this.cityName = city.name; // Aktualisiere den Namen der ausgewählten Stadt
-    this.cities = []; // Leere die Liste der Vorschläge
-  },
-  selectDestination(destination) {
-    this.destinationName = destination.name; // Aktualisiere den Namen der ausgewählten Stadt
-    this.destinations = []; // Leere die Liste der Vorschläge
-  },
-},
   mounted() {
     // Beispiel für das Setzen eines Hintergrundbildes, angepasst an deine Projektstruktur
-    document.documentElement.style.setProperty('--background-image-url', `url(${require('@/assets/background2.png')})`);
+    document.documentElement.style.setProperty(
+      "--background-image-url",
+      `url(${require("@/assets/background2.png")})`
+    );
+  },
+  methods: {
+    async searchFlightsAndHotels() {
+      try {
+        await this.searchFlights(); // Aufruf der Methode zur Flugsuche
+        await this.searchHotels(); // Aufruf der Methode zur Hotelsuche
+      } catch (error) {
+        console.error("Fehler beim Suchen von Flügen und Hotels:", error);
+      }
+    },
+
+    formatDate(date) {
+      return new Date(date).toLocaleString(); // Ändern Sie das Datum in das gewünschte Format
+    },
+    toggleFilter() {
+      // Wenn der aktuelle Filter Flüge sind, ändere ihn auf Hotels und umgekehrt
+      this.filterType = this.filterType === "Flüge" ? "Hotels" : "Flüge";
+    },
+    async searchFlights() {
+      try {
+        // Verwende parseInt, um sicherzustellen, dass die Werte als Zahlen übergeben werden.
+        const response = await axios.post(
+          "http://localhost:3000/search-flights",
+          {
+            departure: this.departure,
+            locationDeparture: this.cityName,
+            locationArrival: this.destinationName,
+            adults: this.adults,
+            children: this.children,
+            infants: this.infants,
+            travelClass: this.travelClass,
+            nonStop: this.nonStop,
+            maxPrice: this.maxPrice,
+            returnDate: this.returnDate, // Entferne das 'arrival' Feld, es wird nicht benötigt.
+          },
+          {
+            headers: { "Content-Type": "application/json" },
+          }
+        );
+        console.log(response.data); // Nach dem Empfang der Antwort von der API
+        this.flights = response.data; // oder response.data.data, je nach Struktur der Antwort
+
+        this.flights = response.data;
+      } catch (error) {
+        console.error("Fehler beim Suchen von Flügen:", error);
+      }
+    },
+
+    async searchHotels() {
+      try {
+        const response = await axios.post(
+          "http://localhost:3000/search-hotels",
+          {
+            location: this.destinationName, // Verwenden Sie den City Code des Zielortes
+          },
+          {
+            headers: { "Content-Type": "application/json" },
+          }
+        );
+
+        console.log(response.data); // Logge die Antwort, um ihre Struktur zu überprüfen
+        this.hotels = response.data; // oder passen Sie diesen Pfad entsprechend der tatsächlichen Antwortstruktur an
+      } catch (error) {
+        console.error("Fehler beim Suchen von Hotels:", error);
+      }
+    },
+
+    async searchCities() {
+      if (this.cityName.length < 3) {
+        this.cities = []; // Leere die Liste, wenn weniger als 3 Buchstaben eingegeben wurden
+        return; // Beende die Methode vorzeitig
+      }
+
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/search-cities?keyword=${this.cityName}`
+        );
+        this.cities = response.data.data; // Aktualisiere die Liste mit den neuen Vorschlägen
+      } catch (error) {
+        console.error("Fehler beim Abrufen der Städte:", error);
+        this.cities = []; // Leere die Liste im Fehlerfall
+      }
+    },
+
+    async searchDestinations() {
+      if (this.destinationName.length < 3) {
+        this.destinations = []; // Leere die Liste, wenn weniger als 3 Buchstaben eingegeben wurden
+        return; // Beende die Methode vorzeitig
+      }
+
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/search-cities?keyword=${this.destinationName}`
+        );
+        this.destinations = response.data.data; // Aktualisiere die Liste mit den neuen Vorschlägen
+      } catch (error) {
+        console.error("Fehler beim Abrufen der Ziele:", error);
+        this.destinations = []; // Leere die Liste im Fehlerfall
+      }
+    },
+    selectCity(city) {
+      this.cityName = city.iataCode; // Setze den IATA-Code als Wert des Input-Feldes
+      this.cities = []; // Leere die Liste der Vorschläge
+    },
+
+    selectDestination(destination) {
+      this.destinationName = destination.iataCode; // Setze den IATA-Code als Wert des Input-Feldes
+      this.destinations = [];
+      this.hotels = []; // Leere die Liste der Vorschläge
+    },
+    toggleAdvancedOptions() {
+      this.showAdvancedOptions = !this.showAdvancedOptions;
+    },
   },
 };
 </script>
 <style>
-html, body {
+html,
+body {
+  font-family: "Brandon Grotesque Black";
   margin: 0;
   padding: 0;
   overflow: hidden; /* Verhindert das Scrollen auf der Seite */
   height: 100%; /* Stellt sicher, dass html und body den gesamten Bildschirm ausfüllen */
   width: 100%; /* Stellt sicher, dass html und body den gesamten Bildschirm ausfüllen */
 }
+.hinflug{
+  padding-left: 25px;
+}
+
+.erwachsene {
+  padding-left: 25px;
+}
+.reiseklasse{
+  padding-bottom: 20px;
+}
 
 .wrapper {
-  position: absolute; /* Geändert zu absolute für eine präzisere Positionierung */
+  position: absolute;
   top: 0;
   left: 0;
-  height: 100vh; /* Nutzt 100% der Viewport-Höhe */
-  width: 100vw; /* Nutzt 100% der Viewport-Breite */
+  height: 100vh;
+  width: 100vw;
   background-image: var(--background-image-url);
   background-size: cover;
   background-position: center;
@@ -126,7 +372,54 @@ html, body {
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
+  overflow-y: auto; /* Vertikalen Scrollbalken anzeigen, wenn der Inhalt zu groß wird */
 }
+
+.digga {
+  background-color: white;
+  border-radius: 5px;
+}
+.Datum {
+  margin-right: 10px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+.flight-list {
+  list-style: none;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px; /* Abstand zwischen den Containern */
+}
+
+.flight-info {
+  margin-top: 50px;
+  margin-right: 10px;
+  width: 200px; /* Breite auf 50% des Containers minus Abstand */
+  max-width: 400px; /* Maximale Breite des Containers */
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 20px; /* Abstand zwischen den Containern */
+  background-color: rgb(51, 50, 48);
+}
+
+.flight-container {
+  display: flex; /* Container als Flex-Container anzeigen */
+  flex-wrap: wrap; /* Elemente umbrechen, wenn nicht genügend Platz vorhanden ist */
+  justify-content: center; /* Zentrieren Sie die Container horizontal */
+}
+
+.segment-info {
+  margin-bottom: 10px;
+}
+
+.flight-detail {
+  color: white; /* Ändern Sie die Farbe nach Ihren Wünschen */
+  font-family: "Brandon Grotesque Black", sans-serif; /* Ändern Sie die Schriftart nach Ihren Wünschen */
+  margin: 5px 0; /* Fügen Sie einen Abstand zwischen den Flugdetails hinzu */
+  margin: 5px 0;
+} /* Fügen Sie einen Abstand zwischen den Flugdetails hinzu */
 
 .navBar {
   font-family: Rockwell;
@@ -142,11 +435,36 @@ html, body {
   z-index: 3;
 }
 
+h4 {
+  margin-right: 20px;
+}
+
 .underline {
   height: 2px;
   background-color: currentColor;
-  box-shadow: 0 2px 5px rgba(0,0,0,0.5);
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
   margin-top: 10px;
+}
+
+.checkbox {
+  margin-left: 5px; /* Kleinerer Abstand zur Checkbox */
+  padding: 0px;
+  width: 50px;
+}
+
+.checkboxLabel {
+  margin-right: 10px;
+  width: auto; /* Entferne eine feste Breite, falls vorhanden */
+  font-weight: bold;
+}
+
+.checkboxGroup {
+  display: flex;
+  align-items: center;
+}
+
+.label {
+  margin-left: 0 !important;
 }
 
 .routePlanner {
@@ -171,6 +489,8 @@ html, body {
   align-items: center; /* Zentriert die Elemente vertikal */
   width: 100%;
   position: relative;
+  margin-bottom: 20px;
+  margin-right: 20px;
 }
 
 .routePlanner input {
@@ -180,10 +500,11 @@ html, body {
   border: none;
   background-color: white; /* Hintergrund zu reinem Weiß für bessere Sichtbarkeit */
   color: black; /* Schriftfarbe zu Schwarz geändert für besseren Kontrast */
+  margin-right: 20px;
 }
 
 .plan {
-  width: 250px;
+  width: 200px;
   margin-top: 20px;
   height: 40px;
   background-color: white; /* Hintergrund zu reinem Weiß für bessere Sichtbarkeit */
@@ -200,7 +521,8 @@ html, body {
   background-color: #cccccc; /* Dunklerer Ton für den Hover-Zustand */
 }
 
-.icon, .buttonIcon {
+.icon,
+.buttonIcon {
   margin-right: 10px;
 }
 
@@ -227,23 +549,30 @@ html, body {
 }
 
 /* Anpassung für das inputGroup-Div, um die absolute Positionierung der Dropdowns zu unterstützen */
+
 .inputGroup {
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: center; /* Zentriert die Elemente vertikal */
+  width: 100%;
   position: relative;
 }
+
+.inputWithIcon {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+
 
 /* Anpassungen für das routePlanner-Div, um Platz für das Dropdown zu schaffen */
 .routePlanner {
   padding-bottom: 80px; /* Erhöhe den unteren Abstand, um Platz für das Dropdown zu schaffen */
 }
 
-.inputWithIcon {
-  display: flex;
-  align-items: center;
-  width: 100%;
-  justify-content: center;
+.travelClassSelect {
+  width: 100%; /* Vollständige Breite für das Select-Element */
+  max-width: 220px; /* Maximale Breite für das Select-Element */
 }
 
 .dropdownContainer {
@@ -254,5 +583,22 @@ html, body {
   left: 50%;
   transform: translateX(-50%); /* Zentriert den Container horizontal */
   z-index: 5;
+}
+
+.hotel-container {
+  margin-top: 20px;
+}
+
+.hotel-info {
+  background-color: #f0f0f0;
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+select {
+  text-align-last: center; /* Zentriert den Text im Dropdown */
+  width: 220px;
+  height: 30px;
 }
 </style>
